41150f3ebec174a2f0f49b9c9cd9e0b6d8de0ea7
cliffud <cliffordjwong@gmail.com>
2013-12-06 01:38:23 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Linkedin::Application.config.secret_key_base = '641d09b26d7b9b27f0769402d5e951d2e043acc52424aa47a0c1759475a721b9849607aeaf28fa6634df38c3d2fda8462ad53790d0076b2445b6a6b98b352539'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Linkedin::Application.config.session_store :cookie_store, key: '_linkedin_session'
